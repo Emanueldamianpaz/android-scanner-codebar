4cd16f9e109109ff9fc8f878828b3803a3c4283a
Emanuel <emanueldamian.paz@gmail.com>
2017-09-03 18:18:54 -0300

    Empezando proyecto@

---
+++ app/src/main/java/com/example/alumno/proyecto01_cudek_rivera_paz/scanner/ScannerActivity.java
@@ -0,0 +1,15 @@
+package com.example.alumno.proyecto01_cudek_rivera_paz.scanner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.alumno.proyecto01_cudek_rivera_paz.R;
+
+public class ScannerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scanner);
+    }
+}
